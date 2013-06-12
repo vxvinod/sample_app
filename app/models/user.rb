a01b12aa08bdd9742590b86ef843033e185a6ada
vxvinod <vinodkmrmurugan@gmail.com>
2013-06-12 09:22:15 +0530

    start from 6.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@
 
 class User < ActiveRecord::Base
   attr_accessible :email , :name
+  before_save { |user| user.email=email.downcase }
   validates :name,presence: true ,length: {maximum: 13}
   VALID_EMAIL_REGX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email,presence: true, format:{ with:VALID_EMAIL_REGX},
