663f751a2c5a8d6292434ab31055fc42a353e8ca
vxvinod <vinodkmrmurugan@gmail.com>
2013-06-13 09:03:02 +0530

    password test has been written

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,12 +13,18 @@ require 'spec_helper'
 
 describe User do
 
-  before { @user = User.new(name: "Example User", email: "user@example.com") }
+  before do 
+  	@user = User.new(name: "Example User", email: "user@example.com",password: "password",password_confirmation: "password") 
+	
+	end  	
 
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_digest)}
+  it { should respond_to (:password)}
+  it { should respond_to(:password_confirmation)}
 
   describe "when name is not present" do
   	before { @user.name=""}
@@ -65,4 +71,20 @@ describe User do
 		it {should_not be_valid}
 	end
 
+	describe "password is not present" do
+		before do
+			@user.password=@user.password_confirmation""
+		end
+		it { should_not be_valid }
+		end	
+		
+		describe "password doesn't match confirmation" do
+		before {@user.password_confirmation="mismatch"}
+		it { should_npt be_valid }
+	end
+
+	describe "password  confirmation is nill" do
+		before { @user.password_confirmation=nil}
+		it{ should_not be_valid}
+	end
 end
