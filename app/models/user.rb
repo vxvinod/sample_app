4ba4625acfa5deaf8830548c73ff54b1e51464f5
vxvinod <vinodkmrmurugan@gmail.com>
2013-06-17 09:02:33 +0530

    Basic user model with secure password is created

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,15 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email , :name
+  attr_accessible :email , :name ,:password ,:password_confirmation
+  has_secure_password
+  
   before_save { |user| user.email=email.downcase }
   validates :name,presence: true ,length: {maximum: 13}
   VALID_EMAIL_REGX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email,presence: true, format:{ with:VALID_EMAIL_REGX},
  					uniqueness:{ case_sensitive: false}
+
+  validates :password, presence: true,length: {minimum: 6}
+  validates :password_confirmation, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,6 +25,8 @@ describe User do
   it { should respond_to(:password_digest)}
   it { should respond_to (:password)}
   it { should respond_to(:password_confirmation)}
+  it { should respond_to(:authenticate)}
+
 
   describe "when name is not present" do
   	before { @user.name=""}
@@ -73,18 +75,39 @@ describe User do
 
 	describe "password is not present" do
 		before do
-			@user.password=@user.password_confirmation""
+			@user.password=@user.password_confirmation=""
 		end
 		it { should_not be_valid }
 		end	
 		
 		describe "password doesn't match confirmation" do
 		before {@user.password_confirmation="mismatch"}
-		it { should_npt be_valid }
+		it { should_not be_valid }
 	end
 
 	describe "password  confirmation is nill" do
 		before { @user.password_confirmation=nil}
 		it{ should_not be_valid}
+	end
+
+	describe "with a password that is too short" do
+		before { @user.password=@user.password_confirmation="a"*5}
+	it {should be_invalid}	
+	end
+
+	describe "return value of authentication method" do
+		before {@user.save}
+		let(:found_user) {User.find_by_email(@user.email)}
+
+		describe "with valid password" do
+			it{ should == found_user.authenticate(@user.password)}
+		end
+
+		describe "with invalid password" do
+		let(:user_for_invalid_password) { found_user.authenticate("invalid")}
+		it{ should_not == user_for_invalid_password}
+
+		specify{ user_for_invalid_password.should be_false }
+		end
 	end
 end
