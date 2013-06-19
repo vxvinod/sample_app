7d727d0be38cac198cf266a4a3380e83810db6ee
vxvinod <vinodkmrmurugan@gmail.com>
2013-06-19 08:40:57 +0530

    User data retrieved from database in show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
   attr_accessible :email , :name ,:password ,:password_confirmation
   has_secure_password
   
-  before_save { |user| user.email=email.downcase }
+  before_save { email.downcase! }
   validates :name,presence: true ,length: {maximum: 13}
   VALID_EMAIL_REGX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email,presence: true, format:{ with:VALID_EMAIL_REGX},
@@ -21,4 +21,5 @@ class User < ActiveRecord::Base
 
   validates :password, presence: true,length: {minimum: 6}
   validates :password_confirmation, presence: true
+
 end
